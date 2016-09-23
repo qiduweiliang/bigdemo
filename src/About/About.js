4736ea8389ec8c641bbd924730617b14126d2c77
qiduweiliang <920049929@qq.com>
2016-09-23 13:52:09 +0800

    fist

--- src/About/About.js
+++ src/About/About.js
@@ -3,8 +3,9 @@ import React, { PropTypes } from 'react'
 class About extends React.Component {
   render () {
     return(
-      <div>
-        About
+      <div className="about-container">
+        <h1>个人信息</h1>
+        <p>喜欢写代码，喜欢打游戏，更喜欢瞎想</p>
       </div>
     )
   }
