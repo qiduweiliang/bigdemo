be4da0a27fc58caf1c74193c33aa66ed9dfe5ab3
qiduweiliang <920049929@qq.com>
2016-09-23 09:45:41 +0800

    fist

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.css'
-import Routes from './Routes'
-
-render(<Routes />,document.getElementById('root'));
